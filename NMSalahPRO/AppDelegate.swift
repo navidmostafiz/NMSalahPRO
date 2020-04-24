4888c539b2d95e764edfe40c9485ba646a76671d
navidmostafiz <navidmostafiz@gmail.com>
2020-04-23 20:20:55 -0400

    Initial Commit

---
+++ NMSalahPRO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NMSalahPRO
+//
+//  Created by Navid on 4/23/20.
+//  Copyright © 2020 UnitedStar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
